982e6a64675d624e1a953dc1d71e77644ae52136
MariaSanchezVivo <m.sanchezvivo@um.es>
2025-12-14 15:58:42 +0100

    Lab Python: 2a parte

---
+++ submit_Python_nikola-alumno23.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=lab4
+#SBATCH --output=lab4_%j.txt
+#SBATCH --partition=hpc-bio-nikola-cpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+module load anaconda/2025.06
+
+echo "Ejecutando 10^7 elementos"
+ipython reduc-operation-alumno23.ipynb 10000000
+
+echo ""
+
+echo "Ejecutando para 10^8 elementos"
+ipython reduc-operation-alumno23.ipynb 100000000
+
